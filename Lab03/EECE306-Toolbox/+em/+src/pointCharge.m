43c1fdab387965e355927ab7c5ebc12dddb6ccc0
MitchellJCodes <Mitchell.Nazareth2@gmail.com>
2026-09-09 12:52:53 -0400

    Update pointCharge.m

--- Lab03/EECE306-Toolbox/+em/+src/pointCharge.m
+++ Lab03/EECE306-Toolbox/+em/+src/pointCharge.m
@@ -1,7 +1,7 @@
 function s = pointCharge(Q, r0)
-%POINTCHARGE Summary of this function goes here
-%   Detailed explanation goes here
+
 if isequal(size(r0), [1 3])
+    % The '.' accesses or creates a field within the struct.
     s.type = 'charge';
     s.pos = r0;
     s.w = 1;
@@ -11,4 +11,3 @@ else
     5
     error('r0 must be a 1x3 position');
 end
-
