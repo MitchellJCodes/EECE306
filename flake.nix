732b991d35d6ba3382489555356c2e2b51b1943e
Mitchell Nazareth <198786687+MitchellJCodes@users.noreply.github.com>
2026-09-09 18:10:26 -0400

    Created flake to enter dev environment

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Development environment for EECE306 labs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          octaveFull
+          ghostscript
+          qt6.qtbase
+        ];
+      };
+    };
+}
